042cd735497d462f791d24e37f452112d9ab6661
YooJinHa96 <yjs19162@hanmail.net>
2021-05-23 19:31:51 +0900

    TD3_LSTM

---
+++ Stock_Trader_TD3_LSTM/critic.py
@@ -0,0 +1,155 @@
+import numpy as np
+import tensorflow as tf
+import keras.backend as K
+
+from keras.initializers import RandomUniform
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.layers import Input, Dense, concatenate, LSTM, Reshape, BatchNormalization, Lambda, Flatten
+
+class CriticNetwork:
+    """ Critic for the DDPG Algorithm, Q-Value function approximator
+    """
+
+    def __init__(self, inp_dim, lr, tau,num_steps):
+        # Dimensions and Hyperparams
+        self.env_dim = inp_dim
+        self.act_dim = 1
+        self.num_steps=num_steps
+        self.tau, self.lr = tau, lr
+        # Build models and target models
+        self.model = self.network()
+        self.target_model = self.network()
+        self.model.compile(Adam(self.lr), 'mse')
+        self.target_model.compile(Adam(self.lr), 'mse')
+        # Function to compute Q-value gradients (Actor Optimization)
+        self.action_grads = K.function([self.model.input[0], self.model.input[1]], K.gradients(self.model.output, [self.model.input[1]]))
+        #self.action_grads = K.function([self.model.input, self.model.input[1]], K.gradients(self.model.output, [self.model.input[1]]))
+
+        #td3
+        self.model2 = self.network()
+        self.target_model2 = self.network()
+
+        self.model2.compile(Adam(self.lr), 'mse')
+        self.target_model2.compile(Adam(self.lr), 'mse')
+
+        self.model3=self.network2()
+
+    def network(self):
+        """ Assemble Critic network to predict q-values
+        """
+        state = Input((self.env_dim * self.num_steps,))
+        action = Input((self.act_dim,))
+        x = Dense(256, activation='relu')(state)
+        x = concatenate([state, action])
+        x = Dense(128, activation='relu')(x)
+        out = Dense(1, activation='linear', kernel_initializer=RandomUniform())(x)
+
+        """
+        state = Input((self.env_dim,))
+        action = Input((self.act_dim,))
+        x = Dense(256, activation='relu')(state)
+        x = concatenate([state, action])
+        x = Dense(128, activation='relu')(x)
+        out = Dense(1, activation='linear', kernel_initializer=RandomUniform())(x)
+        """
+        return Model([state, action], out)
+
+    def network2(self): #지울거
+        """ Assemble Critic network to predict q-values
+        """
+        state = Input((self.num_steps, self.env_dim,))
+        action = Input((self.num_steps, self.act_dim,))
+        output = LSTM(256, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(state)
+        output = BatchNormalization()(output)
+        x = concatenate([output, action])
+        output = LSTM(128, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(x)
+        output = BatchNormalization()(output)
+        output = LSTM(64, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        output = LSTM(32, dropout=0.1,
+                      stateful=False,
+                      kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+
+        """
+        state = Input((self.env_dim,))
+        action = Input((self.act_dim,))
+        x = Dense(256, activation='relu')(state)
+        x = concatenate([state, action])
+        x = Dense(128, activation='relu')(x)
+        out = Dense(1, activation='linear', kernel_initializer=RandomUniform())(x)  
+        """
+        return Model([state, action], output)
+
+
+    def gradients(self, states, actions):
+        """ Compute Q-value gradients w.r.t. states and policy-actions
+        """
+        states = np.array(states).reshape(-1, self.env_dim * self.num_steps)
+        action_s = np.array(actions).reshape(-1, 1)
+        return self.action_grads([states, action_s])
+
+    def target_predict(self, sample, action):
+        """ Predict Q-Values using the target network
+        """
+        sample = np.array(sample).reshape(-1,self.env_dim*self.num_steps)
+        action = np.array(action).reshape(-1,1)
+
+        return self.target_model.predict([sample, action])
+
+    def target_predict2(self, sample, action):
+        """ Predict Q-Values using the target network
+        """
+        sample = np.array(sample).reshape(-1,self.env_dim*self.num_steps)
+        action = np.array(action).reshape(-1,1)
+
+        return self.target_model2.predict([sample, action])
+
+    def predict(self, sample, action):
+        """ Predict Q-Values using the target network
+        """
+        sample = np.array(sample).reshape(-1, self.num_steps, self.env_dim)
+        action = np.array(action).reshape(-1, self.num_steps, 1)
+        return self.model3.predict([sample, action])
+
+    def train_on_batch(self, samples, actions, critic_target):
+        """ Train the critic network on batch of sampled experience
+        """
+        loss = 0
+        loss1 = 0
+        loss2 = 0
+        samples = np.array(samples).reshape(-1, self.env_dim*self.num_steps)
+        actions = np.array(actions).reshape(-1, 1)
+
+        loss += self.model.train_on_batch([samples, actions], critic_target)
+        loss += self.model2.train_on_batch([samples, actions], critic_target)
+        return loss
+
+    def transfer_weights(self):
+        """ Transfer model weights to target model with a factor of Tau
+        """
+        W, target_W = self.model.get_weights(), self.target_model.get_weights()
+        for i in range(len(W)):
+            target_W[i] = self.tau * W[i] + (1 - self.tau)* target_W[i]
+        self.target_model.set_weights(target_W)
+
+        W, target_W = self.model2.get_weights(), self.target_model2.get_weights()
+        for i in range(len(W)):
+            target_W[i] = self.tau * W[i] + (1 - self.tau) * target_W[i]
+        self.target_model2.set_weights(target_W)
+
+    def save_model(self, model_path):
+        self.model.save_weights(model_path)
+        # self.target_model.save_weights(path)
+
+    def load_model(self, model_path):
+        self.model.load_weights(model_path)
+    # self.target_model.load_weights(path)
+
---
+++ Stock_Trader_TD3_LSTM/networks.py
@@ -0,0 +1,483 @@
+import os
+import threading
+import numpy as np
+import argparse
+
+
+class DummyGraph:
+    def as_default(self): return self
+
+    def __enter__(self): pass
+
+    def __exit__(self, type, value, traceback): pass
+
+
+def set_session(sess): pass
+
+
+graph = DummyGraph()
+sess = None
+
+if os.environ['KERAS_BACKEND'] == 'tensorflow':
+
+    from tensorflow.keras.models import Model
+    from tensorflow.keras.layers import Input, Dense, LSTM, Conv2D, \
+        BatchNormalization, Dropout, MaxPooling2D, Flatten, concatenate
+    #from keras.initializers import RandomUniform
+    from tensorflow.keras.optimizers import SGD
+   # from tensorflow.keras.backend import set_session
+    import tensorflow.keras.backend as K
+    import tensorflow.compat.v1 as tf
+    tf.disable_v2_behavior()
+    print(tf.__version__)
+    """
+    Tensor v2 
+    import tensorflow.compat.v1 as tf
+
+    tf.disable_v2_behavior()
+    """
+    graph = tf.get_default_graph()
+    sess = tf.compat.v1.Session()
+#elif os.environ['KERAS_BACKEND'] == 'plaidml.keras.backend':
+ #   from keras.models import Model
+  #  from keras.layers import Input, Dense, LSTM, Conv2D, \
+   #     BatchNormalization, Dropout, MaxPooling2D, Flatten
+   # from keras.layers import concatenate
+   # from keras.initializers import RandomUniform
+    #from keras.optimizers import SGD
+
+
+class Network:
+    lock = threading.Lock()
+
+    def __init__(self, input_dim=0, output_dim=0, lr=0.001,
+                 shared_network=None, activation='sigmoid', loss='mse'):
+
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.lr = lr
+        self.shared_network = shared_network
+        self.activation = activation
+        self.loss = loss
+        self.model = None
+        self.target_model = None
+
+    def predict(self, sample):
+        with self.lock:
+            with graph.as_default():
+                if sess is not None:
+                    set_session(sess)
+                return self.model.predict(sample).flatten()
+
+    def target_predict(self, sample):
+        with self.lock:
+            with graph.as_default():
+                if sess is not None:
+                    set_session(sess)
+                return self.target_model.predict(sample).flatten()
+
+    def train_on_batch(self, x, y):
+        loss = 0.
+        with self.lock:
+            with graph.as_default():
+                if sess is not None:
+                    set_session(sess)
+                loss = self.model.train_on_batch(x, y)
+        return loss
+
+    def optimizer(self):
+        """ Actor Optimizer
+        """
+        action_gdts = K.placeholder(shape=(None, self.output_dim))
+        params_grad = tf.gradients(self.model.output, self.model.trainable_weights, -action_gdts)
+        grads = zip(params_grad, self.model.trainable_weights)
+        return K.function([self.model.input, action_gdts], [tf.train.AdamOptimizer(self.lr).apply_gradients(grads)])
+
+    def save_model(self, model_path):
+        if model_path is not None and self.model is not None:
+            self.model.save_weights(model_path, overwrite=True)
+
+    def load_model(self, model_path):
+        if model_path is not None:
+            self.model.load_weights(model_path)
+
+    @classmethod
+    def get_shared_network(cls, net='dnn', num_steps=1, input_dim=0):
+        with graph.as_default():
+            if sess is not None:
+                set_session(sess)
+            if net == 'dnn':
+                return DNN.get_network_head(Input((input_dim,)))
+            elif net == 'lstm':
+                return LSTMNetwork.get_network_head(
+                    Input((num_steps, input_dim)))
+            elif net == 'cnn':
+                return CNN.get_network_head(
+                    Input((1, num_steps, input_dim)))
+
+
+class DNN(Network):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        with graph.as_default():
+            if sess is not None:
+                set_session(sess)
+            inp = None
+            output = None
+            if self.shared_network is None:
+                inp = Input((self.input_dim,))
+                output = self.get_network_head(inp).output
+            else:
+                inp = self.shared_network.input
+                output = self.shared_network.output
+            output = Dense(
+                self.output_dim, activation=self.activation,
+                kernel_initializer='random_normal')(output)
+            self.model = Model(inp, output)
+            self.model.compile(
+                optimizer=SGD(lr=self.lr), loss=self.loss)
+
+    @staticmethod
+    def get_network_head(inp):
+        output = Dense(256, activation='sigmoid',
+                       kernel_initializer='random_normal')(inp)
+        output = BatchNormalization()(output)
+        output = Dropout(0.1)(output)
+        output = Dense(128, activation='sigmoid',
+                       kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        output = Dropout(0.1)(output)
+        output = Dense(64, activation='sigmoid',
+                       kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        output = Dropout(0.1)(output)
+        output = Dense(32, activation='sigmoid',
+                       kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        output = Dropout(0.1)(output)
+        return Model(inp, output)
+
+    def train_on_batch(self, x, y):
+        x = np.array(x).reshape((-1, self.input_dim))
+        return super().train_on_batch(x, y)
+
+    def predict(self, sample):
+
+        sample = np.array(sample).reshape((1, self.input_dim))
+        return super().predict(sample)
+
+
+class LSTMNetwork(Network):
+    def __init__(self, *args, num_steps=1, **kwargs):
+        super().__init__(*args, **kwargs)
+        with graph.as_default():
+            if sess is not None:
+                set_session(sess)
+            self.num_steps = num_steps
+            inp = None
+            output = None
+            if self.shared_network is None:
+                inp = Input((self.num_steps, self.input_dim))
+                output = self.get_network_head(inp).output
+            else:
+                inp = self.shared_network.input
+                output = self.shared_network.output
+            output = Dense(
+                self.output_dim, activation=self.activation,
+                kernel_initializer='random_normal')(output)
+            self.model = Model(inp, output)
+            self.model.compile(
+                optimizer=SGD(lr=self.lr), loss=self.loss)
+
+    @staticmethod
+    def get_network_head(inp):
+        output = LSTM(256, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(inp)
+        output = BatchNormalization()(output)
+        output = LSTM(128, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        output = LSTM(64, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        output = LSTM(32, dropout=0.1,
+                      stateful=False,
+                      kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        return Model(inp, output)
+
+    def train_on_batch(self, x, y):
+        x = np.array(x).reshape((-1, self.num_steps, self.input_dim))
+        return super().train_on_batch(x, y)
+
+    def predict(self, sample):
+        sample = np.array(sample).reshape(
+            (1, self.num_steps, self.input_dim))
+        return super().predict(sample)
+
+
+class CNN(Network):
+    def __init__(self, *args, num_steps=1, **kwargs):
+        super().__init__(*args, **kwargs)
+        with graph.as_default():
+            if sess is not None:
+                set_session(sess)
+            self.num_steps = num_steps
+            inp = None
+            output = None
+            if self.shared_network is None:
+                inp = Input((self.num_steps, self.input_dim, 1))
+                output = self.get_network_head(inp).output
+            else:
+                inp = self.shared_network.input
+                output = self.shared_network.output
+            output = Dense(
+                self.output_dim, activation=self.activation,
+                kernel_initializer='random_normal')(output)
+            self.model = Model(inp, output)
+            self.model.compile(
+                optimizer=SGD(lr=self.lr), loss=self.loss)
+
+    @staticmethod
+    def get_network_head(inp):
+        output = Conv2D(256, kernel_size=(1, 5),
+                        padding='same', activation='sigmoid',
+                        kernel_initializer='random_normal')(inp)
+        output = BatchNormalization()(output)
+        output = MaxPooling2D(pool_size=(1, 2))(output)
+        output = Dropout(0.1)(output)
+        output = Conv2D(128, kernel_size=(1, 5),
+                        padding='same', activation='sigmoid',
+                        kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        output = MaxPooling2D(pool_size=(1, 2))(output)
+        output = Dropout(0.1)(output)
+        output = Conv2D(64, kernel_size=(1, 5),
+                        padding='same', activation='sigmoid',
+                        kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        output = MaxPooling2D(pool_size=(1, 2))(output)
+        output = Dropout(0.1)(output)
+        output = Conv2D(32, kernel_size=(1, 5),
+                        padding='same', activation='sigmoid',
+                        kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        output = MaxPooling2D(pool_size=(1, 2))(output)
+        output = Dropout(0.1)(output)
+        output = Flatten()(output)
+        return Model(inp, output)
+
+    def train_on_batch(self, x, y):
+        x = np.array(x).reshape((-1, self.num_steps, self.input_dim, 1))
+        return super().train_on_batch(x, y)
+
+    def predict(self, sample):
+        sample = np.array(sample).reshape(
+            (-1, self.num_steps, self.input_dim, 1))
+        return super().predict(sample)
+
+
+class ActorNetwork(Network):
+    def __init__(self, *args, num_steps=1, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.tau = 0.001
+
+        with graph.as_default():
+            if sess is not None:
+                set_session(sess)
+            self.num_steps = num_steps
+            inp = None
+            output = None
+            output2 = None
+            if self.shared_network is None:
+                inp = Input((self.num_steps, self.input_dim))
+                output = self.get_network_head(inp).output
+                output2 = self.get_network_head2(inp).output
+            else:
+                inp = self.shared_network.input
+                output = self.shared_network.output
+            output = Dense(
+                self.output_dim, activation=self.activation,
+                kernel_initializer='random_normal')(output)
+            output2 = Dense(
+                self.output_dim, activation=self.activation,
+                kernel_initializer='random_normal')(output2)    
+              
+            self.model = Model(inp, output)
+            self.target_model = Model(inp, output2)
+            self.model.compile(
+                optimizer=SGD(lr=self.lr), loss=self.loss)
+            self.target_model.compile(
+                optimizer=SGD(lr=self.lr), loss=self.loss)
+            self.adam_optimizer = self.optimizer()
+
+    @staticmethod
+    def get_network_head(inp):
+        output = LSTM(256, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(inp)
+        output = BatchNormalization()(output)
+        output = LSTM(128, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        output = LSTM(64, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        output = LSTM(32, dropout=0.1,
+                      stateful=False,
+                      kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        return Model(inp, output)
+    @staticmethod
+    def get_network_head2(inp):
+        output2 = LSTM(256, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(inp)
+        output2 = BatchNormalization()(output2)
+        output2 = LSTM(128, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(output2)
+        output2 = BatchNormalization()(output2)
+        output2 = LSTM(64, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(output2)
+        output2 = BatchNormalization()(output2)
+        output2 = LSTM(32, dropout=0.1,
+                      stateful=False,
+                      kernel_initializer='random_normal')(output2)
+        output2 = BatchNormalization()(output2)
+        return Model(inp, output2)
+
+    def train_on_batch(self, x, y):
+        x = np.array(x).reshape((-1, self.num_steps, self.input_dim))
+        return super().train_on_batch(x, y)
+
+    def predict(self, sample):
+        sample = np.array(sample).reshape(
+            (1, self.num_steps, self.input_dim))
+        return super().predict(sample)
+
+    def target_predict(self, sample):
+        """ Action prediction (target network)
+        """
+        sample = np.array(sample).reshape(
+            (1, self.num_steps, self.input_dim))
+        return super().target_predict(sample)
+
+    def optimizer(self):
+        """ Actor Optimizer
+        """
+        action_gdts = K.placeholder(shape=(None, self.output_dim))
+        params_grad = tf.gradients(self.model.output, self.model.trainable_weights, -action_gdts)
+        grads = zip(params_grad, self.model.trainable_weights)
+        return K.function([self.model.input, action_gdts], [tf.train.AdamOptimizer(self.lr).apply_gradients(grads)])
+
+    def transfer_weights(self):
+        """ Transfer model weights to target model with a factor of Tau
+        """
+        W, target_W = self.model.get_weights(), self.target_model.get_weights()
+        for i in range(len(W)):
+            target_W[i] = self.tau * W[i] + (1 - self.tau) * target_W[i]
+        self.target_model.set_weights(target_W)
+
+
+class CriticNetwork(Network):
+    def __init__(self, *args, num_steps=1, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.tau = 0.001
+        with graph.as_default():
+            if sess is not None:
+                set_session(sess)
+            self.num_steps = num_steps
+            inp = None
+            output = None
+            output2 = None
+            if self.shared_network is None:
+                inp = Input((self.num_steps, self.input_dim))
+                output = self.get_network_head(inp).output
+                output2 = self.get_network_head2(inp).output
+            else:
+                inp = self.shared_network.input
+                output = self.shared_network.output
+            output = Dense(
+                self.output_dim, activation=self.activation,
+                kernel_initializer='random_normal')(output)
+            output2 = Dense(
+                self.output_dim, activation=self.activation,
+                kernel_initializer='random_normal')(output2)    
+            self.model = Model(inp, output)
+            self.target_model = Model(inp, output2)
+            self.model.compile(
+                optimizer=SGD(lr=self.lr), loss=self.loss)
+            self.target_model.compile(
+                optimizer=SGD(lr=self.lr), loss=self.loss)
+
+    @staticmethod
+    def get_network_head(inp):
+
+        output = LSTM(256, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(inp)
+        output = BatchNormalization()(output)
+        output = LSTM(128, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(output)
+        output = BatchNormalization()(output)
+        output = LSTM(64, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(output)
+        output = LSTM(32, dropout=0.1,
+                      stateful=False,
+                      kernel_initializer='random_normal')(output)
+
+
+
+        output = BatchNormalization()(output)
+        return Model(inp, output)
+    @staticmethod
+    def get_network_head2(inp):
+        output2 = LSTM(256, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(inp)
+        output2 = BatchNormalization()(output2)
+
+        output2 = LSTM(128, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(output2)
+        output2 = BatchNormalization()(output2)
+        output2 = LSTM(64, dropout=0.1,
+                      return_sequences=True, stateful=False,
+                      kernel_initializer='random_normal')(output2)
+        output2 = LSTM(32, dropout=0.1,
+                      stateful=False,
+                      kernel_initializer='random_normal')(output2)
+
+        output2 = BatchNormalization()(output2)
+        return Model(inp, output2)
+
+    def predict(self, sample):
+        sample = np.array(sample).reshape(
+            (1, self.num_steps, self.input_dim))
+        return super().predict(sample)
+
+    def train_on_batch(self, x, y):
+        x = np.array(x).reshape((-1, self.num_steps, self.input_dim))
+        return super().train_on_batch(x, y)
+
+    def target_predict(self, sample):
+        sample = np.array(sample).reshape(
+            (1, self.num_steps, self.input_dim))
+        return super().target_predict(sample)
+
+    def transfer_weights(self):
+        """ Transfer model weights to target model with a factor of Tau
+        """
+        W, target_W = self.model.get_weights(), self.target_model.get_weights()
+        for i in range(len(W)):
+            target_W[i] = self.tau * W[i] + (1 - self.tau) * target_W[i]
+        self.target_model.set_weights(target_W)
